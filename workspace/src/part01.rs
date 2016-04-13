9d4842d35d8956a9ba1717bdf5e5c4fd6b1d7d45
Boris Egorov <egorov@linux.com>
2016-04-13 18:01:55 +0600

    part01: Use Option instead our own enum

--- workspace/src/part01.rs
+++ workspace/src/part01.rs
@@ -8,55 +8,33 @@ fn sqr(i: i32) -> i32 { i * i }
 // from languages like C.
 fn abs(i: i32) -> i32 { if i >= 0 { i } else { -i } }
 
-enum NumberOrNothing {
-    Number(i32),
-    Nothing
-}
-use self::NumberOrNothing::{Number,Nothing};
-
-// ## Inherent implementations
-impl NumberOrNothing {
-    fn print(self) {
-        match self {
-            Nothing => println!("The number is: <nothing>"),
-            Number(n) => println!("The number is: {}", n),
-        };
-    }
-    fn number_or_default(self, default: i32) -> i32 {
-        match self {
-            Nothing => default,
-            Number(n) => n,
-        }
-    }
-}
-
 // It is even the case that blocks are expressions, evaluating to the last expression they contain.
 fn compute_stuff(x: i32) -> i32 {
     let y = { let z = x*x; z + 14 };
     y*y
 }
 
-fn vec_min(v: &Vec<i32>) -> NumberOrNothing {
+fn vec_min(v: &Vec<i32>) -> Option<i32> {
     fn min_i32(a: i32, b: i32) -> i32 {
         if a < b { a } else { b }
     }
 
-    let mut min = Nothing;
+    let mut min = None;
     for e in v {
-        min = Number(match min {
-            Nothing => *e,
-            Number(n) => min_i32(n, *e)
+        min = Some(match min {
+            None => *e,
+            Some(n) => min_i32(n, *e)
         })
     }
     min
 }
 
-fn vec_sum(v: &Vec<i32>) -> NumberOrNothing {
-    let mut sum = Nothing;
+fn vec_sum(v: &Vec<i32>) -> Option<i32> {
+    let mut sum = None;
     for item in v {
         sum = match sum {
-            Nothing => Number(*item),
-            Number(n) => Number(*item + n),
+            None => Some(*item),
+            Some(n) => Some(*item + n),
         }
     }
     sum
@@ -76,8 +54,8 @@ fn read_vec() -> Vec<i32> {
 pub fn main() {
     let vec = read_vec();
     let min = vec_min(&vec);
-    min.print();
+    println!("Minimum of a vec: {:?}", min);
     let sum = vec_sum(&vec);
-    sum.print();
+    println!("Sum of a vec: {:?}", sum);
     vec_print(&vec);
 }
