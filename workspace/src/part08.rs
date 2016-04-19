38aa15d9fb9eb00ff34cebc163a1499f130ea594
Boris Egorov <egorov@linux.com>
2016-04-19 16:23:04 +0600

    part08: Implement trait Add and tests for it

--- workspace/src/part08.rs
+++ workspace/src/part08.rs
@@ -11,19 +11,20 @@ fn overflowing_add(a: u64, b: u64, carry: bool) -> (u64, bool) {
     // If an overflow happened, then the sum will be smaller than *both* summands. Without an overflow, of course, it will be
     // at least as large as both of them. So, let's just pick one and check.
     if sum >= a {
-        // The addition did not overflow. <br/>
-        // **Exercise 08.1**: Write the code to handle adding the carry in this case.
-        unimplemented!()
+        if carry {
+            let carried_sum = sum.wrapping_add(1);
+            (carried_sum, carried_sum <= sum)
+        } else {
+            (sum, false)
+        }
     } else {
-        // Otherwise, the addition *did* overflow. It is impossible for the addition of the carry
-        // to overflow again, as we are just adding 0 or 1.
-        unimplemented!()
+        (sum + if carry { 1 } else { 0 }, true)
     }
 }
 
 // `overflow_add` is a sufficiently intricate function that a test case is justified.
 // This should also help you to check your solution of the exercise.
-/*#[test]*/
+#[test]
 fn test_overflowing_add() {
     assert_eq!(overflowing_add(10, 100, false), (110, false));
     assert_eq!(overflowing_add(10, 100, true), (111, false));
@@ -33,29 +34,6 @@ fn test_overflowing_add() {
 }
 
 // ## Associated Types
-impl ops::Add<BigInt> for BigInt {
-
-    // Here, we choose the result type to be again `BigInt`.
-    type Output = BigInt;
-
-    // Now we can write the actual function performing the addition.
-    fn add(self, rhs: BigInt) -> Self::Output {
-        // We know that the result will be *at least* as long as the longer of the two operands,
-        // so we can create a vector with sufficient capacity to avoid expensive reallocations.
-        let max_len = cmp::max(self.data.len(), rhs.data.len());
-        let mut result_vec:Vec<u64> = Vec::with_capacity(max_len);
-        let mut carry = false; /* the current carry bit */
-        for i in 0..max_len {
-            let lhs_val = if i < self.data.len() { self.data[i] } else { 0 };
-            let rhs_val = if i < rhs.data.len() { rhs.data[i] } else { 0 };
-            // Compute next digit and carry. Then, store the digit for the result, and the carry for later.
-            unimplemented!()
-        }
-        // **Exercise 08.2**: Handle the final `carry`, and return the sum.
-        unimplemented!()
-    }
-}
-
 // ## Traits and reference types
 
 // Writing this out becomes a bit tedious, because trait implementations (unlike functions) require full explicit annotation
@@ -64,12 +42,52 @@ impl ops::Add<BigInt> for BigInt {
 impl<'a, 'b> ops::Add<&'a BigInt> for &'b BigInt {
     type Output = BigInt;
     fn add(self, rhs: &'a BigInt) -> Self::Output {
-        // **Exercise 08.3**: Implement this function.
-        unimplemented!()
+        // We know that the result will be *at least* as long as the longer of the two operands,
+        // so we can create a vector with sufficient capacity to avoid expensive reallocations.
+        let max_len = cmp::max(self.data.len(), rhs.data.len());
+        let mut result_vec:Vec<u64> = Vec::with_capacity(max_len);
+        let mut carry = false; /* the current carry bit */
+        for i in 0..max_len {
+            let lhs_val = if i < self.data.len() { self.data[i] } else { 0 };
+            let rhs_val = if i < rhs.data.len() { rhs.data[i] } else { 0 };
+            
+            let (sum, new_carry) = overflowing_add(lhs_val, rhs_val, carry);
+            result_vec.push(sum);
+            carry = new_carry;
+        }
+        if carry {
+            result_vec.push(1);
+        }
+        BigInt { data: result_vec }
     }
 }
 
-// **Exercise 08.4**: Implement the two missing combinations of arguments for `Add`. You should not have to duplicate the implementation.
+impl<'a> ops::Add<&'a BigInt> for BigInt {
+    type Output = BigInt;
+
+    #[inline]
+    fn add(self, rhs: &'a BigInt) -> Self::Output {
+        &self + rhs
+    }
+}
+
+impl<'b> ops::Add<BigInt> for &'b BigInt {
+    type Output = BigInt;
+
+    #[inline]
+    fn add(self, rhs: BigInt) -> Self::Output {
+        self + &rhs
+    }
+}
+
+impl ops::Add<BigInt> for BigInt {
+    type Output = BigInt;
+
+    #[inline]
+    fn add(self, rhs: BigInt) -> Self::Output {
+        &self + &rhs
+    }
+}
 
 // ## Modules
 
@@ -78,16 +96,23 @@ impl<'a, 'b> ops::Add<&'a BigInt> for &'b BigInt {
 mod tests {
     use part05::BigInt;
 
-    /*#[test]*/
+    #[test]
     fn test_add() {
         let b1 = BigInt::new(1 << 32);
         let b2 = BigInt::from_vec(vec![0, 1]);
 
         assert_eq!(&b1 + &b2, BigInt::from_vec(vec![1 << 32, 1]));
-        // **Exercise 08.5**: Add some more cases to this test.
+        let max = !0 as u64;
+        let b63 = BigInt::from_vec(vec![max, max, max]);
+        let b001 = BigInt::from_vec(vec![1]);
+        assert_eq!(&b001 + &b63, BigInt::from_vec(vec![0, 0, 0, 1]));
+        assert_eq!(&b63 + &b001, BigInt::from_vec(vec![0, 0, 0, 1]));
+        assert_eq!(BigInt::new(0) + BigInt::new(0), BigInt::new(0));
     }
 }
 
 // **Exercise 08.6**: Write a subtraction function, and testcases for it. Decide for yourself how you want to handle negative results.
 // For example, you may want to return an `Option`, to panic, or to return `0`.
 
+pub fn main() {
+}
